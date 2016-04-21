98ea9332e48b2a845c6962a911867cc1b6b86a51
Or Koren <orkoren5@gmail.com>
2016-04-21 12:16:05 +0300

    Manager: adding reduce methods

--- Manager.java
+++ Manager.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -18,13 +19,13 @@ public class Manager {
 	private AWSHandler 	aws;
 	private  boolean isTerminate = false; 
 	private int tweetsPerWorker;
-	private Map<String, Integer> expectedResultsNum;
+	private Map<String, Integer> expectedResultsNum = new HashMap<String, Integer>();	
 	
 	public Manager() {
 		aws = new AWSHandler();
 	}
 	
-	private void pullMessageAndDoTask() {
+	private void pullMessageAndDeliverTask() {
 		 System.out.println("::MANAGER:: Get file from local");
 
 	        Message message = aws.pullMessageFromSQS(QueueType.LocalToManager);
@@ -38,30 +39,49 @@ public class Manager {
 	            return;
 	        } 
 	        else {
+	        	
+	        	// Distribute the task to tweets and put them in SQS
 	        	String fileNameInS3 = message.getBody();
-	        	int numTweets = distributeWork(aws.downloadFileFromS3(fileNameInS3));
+	        	String taskID = "task_" + UUID.randomUUID().toString();
+	        	int numTweets = distributeWork(taskID, aws.downloadFileFromS3(fileNameInS3));
+	        	expectedResultsNum.put(taskID, numTweets);
+	        		        
+	        	// Delete the task from SQS
 	        	aws.deleteMessageFromSQS(message, QueueType.LocalToManager);
+	        	
+	        	// Initiate workers to do the task
 	        	tweetsPerWorker = Integer.parseInt(message.getMessageAttributes().get("tweetsPerWorker").getStringValue());
 	        	int numWorkers = (int)Math.ceil(numTweets/(double)tweetsPerWorker);
 	        	aws.startWorkers(numWorkers);
 	        }
 	}
 
-	private void reduce() {
-		Message message = null;		
+	private void checkIfTaskFinished() {
+		Message message = null;			
+		Map<String, Integer> receivedResultsNum = new HashMap<String, Integer>();
+		
 		while ((message = aws.pullMessageFromSQS(QueueType.WorkerToManager)) != null) {
-			
+			String taskID = message.getMessageAttributes().get("taskID").getStringValue();
+			int num = receivedResultsNum.get(taskID).intValue();
+			receivedResultsNum.put(taskID, ++num);
+			if (expectedResultsNum.get(taskID).intValue() == num) {
+				reduce(taskID);
+			}
 		}
 	}
+	
+	private void reduce(String taskID) {
+		
+	}
+	
 	/**
 	 * push every tweet in the file to SQS, and returns the number of workers necessary for that job
 	 * @param tweetsFile file input streem of tweets from S3
 	 * @return number of tweets
 	 */
-	private int distributeWork(InputStream tweetsFile) {	
+	private int distributeWork(String taskID, InputStream tweetsFile) {	
 		try {				
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(tweetsFile));
-	        String taskID = "task_" + UUID.randomUUID().toString();
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(tweetsFile));	        
 	        String line;
 	        List<Message> messages = new ArrayList<Message>();
 	        int countTweets = 0;
@@ -96,7 +116,7 @@ public class Manager {
 	public static void main(String[] args) {		
 		Manager manager = new Manager();
 		while (!manager.isTerminate) {
-            manager.pullMessageAndDoTask();
+            manager.pullMessageAndDeliverTask();
         }
         manager.terminateManager();		
 	} 
