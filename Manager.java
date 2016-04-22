502a33dfaded30f556b9c176bbb7906f6a51b231
Or Koren <orkoren5@gmail.com>
2016-04-22 10:06:06 +0300

    Manager: merge

--- Manager.java
+++ Manager.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
